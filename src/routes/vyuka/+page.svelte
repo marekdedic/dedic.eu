1506477b3d248e0a1fd594d5f1e22413443fada0
Marek Dědič <developer@dedic.eu>
2023-10-03 11:46:04 +0200

    Added google sheet embed

---
+++ src/routes/vyuka/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" strictEvents>
+  import GoogleSheet from "../../lib/components/GoogleSheet.svelte";
+</script>
+
+<GoogleSheet
+  sheetId="2096801087"
+  title="Přehled výsledků"
+  url="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOlPd-nlIljrCz_NIvkTFehlMJzedOHIClP1CSohavxk7hcTK-BRC4-wJXJ7KJ5Y3f6sMIzXduTxG0/pubhtml"
+/>
