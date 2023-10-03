7c6e1433da117cc9c25effcf88ef466b1d7a6683
Marek Dědič <developer@dedic.eu>
2023-10-03 16:05:16 +0200

    Split out HeaderName component

---
+++ src/lib/components/HeaderName.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" strictEvents>
+  import Name from "./Name.svelte";
+
+  export let first: string;
+  export let last: string;
+</script>
+
+<a href="/">
+  <Name {first} {last} />
+</a>
+
+<style lang="scss">
+  @use "../theme.scss";
+
+  a {
+    color: theme.$text-color;
+    font-size: 1.25rem;
+    margin-bottom: .3125rem;
+    margin-right: 1rem;
+    margin-top: .3125rem;
+    white-space: nowrap;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+</style>
