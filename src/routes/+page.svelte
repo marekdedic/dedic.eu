e869b043c0eed9360b9f7978336202757ed34742
Marek Dědič <developer@dedic.eu>
2023-10-03 14:52:10 +0200

    Moved layout to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script lang="ts" strictEvents>
-  import "@fontsource/roboto/300.css";
-  import "@fontsource/roboto/300-italic.css";
-  import "@fontsource/roboto/400.css";
-  import "@fontsource/roboto/400-italic.css";
-
-  import Header from "$lib/components/Header.svelte";
 </script>
 
-<Header />
 <div>
   <b>Hello</b>
 
@@ -49,21 +42,6 @@
 </div>
 
 <style>
-  :global(body) {
-    font-family: "Roboto", sans-serif;
-    font-size: 1rem;
-    font-weight: 300;
-    line-height: 1.5;
-  }
-
-  :global(*) {
-    box-sizing: border-box;
-  }
-
-  :global(svg.svelte-fa) {
-    height: 1rem !important;
-  }
-
   div {
     margin-top: 3rem;
     margin-left: auto;
