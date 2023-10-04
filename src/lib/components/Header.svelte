74f1703feea518623b5bab0cece477dd84c5b094
Marek Dědič <developer@dedic.eu>
2023-10-04 19:15:10 +0200

    Split Navigation out of Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,11 +1,8 @@
 <script lang="ts" strictEvents>
   import { page } from "$app/stores";
 
-  import { faMoon } from "@fortawesome/free-solid-svg-icons";
-  import Fa from "svelte-fa";
-
   import HeaderName from "./HeaderName.svelte";
-  import NavigationItem from "./NavigationItem.svelte";
+  import Navigation from "./Navigation.svelte";
 
   $: isBase = $page.route.id === "/";
 </script>
@@ -16,36 +13,13 @@
       {#if !isBase}
         <HeaderName first="You" last="R. Name" />
       {/if}
-      <ul>
-        <NavigationItem href="/" title="about" />
-        <NavigationItem href="/blog" title="blog" />
-        <NavigationItem href="/publications" title="publications" />
-        <NavigationItem href="/projects" title="projects" />
-        <NavigationItem href="/repositories" title="repositories" />
-        <NavigationItem href="/cv" title="cv" />
-        <NavigationItem href="/teaching" title="teaching" />
-        <NavigationItem href="/people" title="people" />
-        <NavigationItem href="/submenus" title="submenus" />
-        <li>
-          <button type="button">
-            <Fa icon={faMoon} />
-          </button>
-        </li>
-      </ul>
+      <Navigation />
     </div>
   </nav>
 </header>
 
 <style lang="scss">
   @use "../theme.scss";
-
-  button {
-    background-color: inherit;
-    border: 0;
-    padding: 0;
-    padding-left: 10px;
-    padding-top: 12px;
-  }
 
   nav {
     background-color: theme.$background-color;
@@ -55,18 +29,6 @@
     top: 0;
     right: 0;
     left: 0;
-  }
-
-  ul {
-    display: flex;
-    margin-bottom: 0;
-    margin-left: auto;
-    margin-top: 0;
-    padding-left: 0;
-  }
-
-  li {
-    list-style: none;
   }
 
   .container {
---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,45 @@
+<script lang="ts" strictEvents>
+  import { faMoon } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
+  import NavigationItem from "./NavigationItem.svelte";
+</script>
+
+<ul>
+  <NavigationItem href="/" title="about" />
+  <NavigationItem href="/blog" title="blog" />
+  <NavigationItem href="/publications" title="publications" />
+  <NavigationItem href="/projects" title="projects" />
+  <NavigationItem href="/repositories" title="repositories" />
+  <NavigationItem href="/cv" title="cv" />
+  <NavigationItem href="/teaching" title="teaching" />
+  <NavigationItem href="/people" title="people" />
+  <NavigationItem href="/submenus" title="submenus" />
+  <li>
+    <button type="button">
+      <Fa icon={faMoon} />
+    </button>
+  </li>
+</ul>
+
+<style>
+  button {
+    background-color: inherit;
+    border: 0;
+    padding: 0;
+    padding-left: 10px;
+    padding-top: 12px;
+  }
+
+  ul {
+    display: flex;
+    margin-bottom: 0;
+    margin-left: auto;
+    margin-top: 0;
+    padding-left: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+</style>
