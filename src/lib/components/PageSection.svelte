afd94909e226fc337ef1b25f18da101400d8b46f
Marek Dědič <developer@dedic.eu>
2023-10-04 18:08:40 +0200

    Added PageSection component

---
+++ src/lib/components/PageSection.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" strictEvents>
+  interface $$Slots {
+    default: Record<string, never>;
+  }
+</script>
+
+<div>
+  <slot />
+</div>
+
+<style>
+  div::after {
+    clear: both;
+    content: "";
+    display: block;
+  }
+</style>
