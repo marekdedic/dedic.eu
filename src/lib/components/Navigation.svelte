9f3d356afe508c69653e6e164ae8bbf1c2b61d1a
Marek Dědič <developer@dedic.eu>
2023-10-05 11:17:54 +0200

    Added mobile navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,34 +1,62 @@
 <script lang="ts" strictEvents>
-  import { faMoon } from "@fortawesome/free-solid-svg-icons";
+  import { faBars, faMoon, faXmark } from "@fortawesome/free-solid-svg-icons";
   import Fa from "svelte-fa";
 
+  import MediaQuery from "./MediaQuery.svelte";
   import NavigationItem from "./NavigationItem.svelte";
+
+  let expandNavigation = false;
 </script>
 
-<ul>
-  <NavigationItem href="/" title="about" />
-  <NavigationItem href="/blog" title="blog" />
-  <NavigationItem href="/publications" title="publications" />
-  <NavigationItem href="/projects" title="projects" />
-  <NavigationItem href="/repositories" title="repositories" />
-  <NavigationItem href="/cv" title="cv" />
-  <NavigationItem href="/teaching" title="teaching" />
-  <NavigationItem href="/people" title="people" />
-  <NavigationItem href="/submenus" title="submenus" />
-  <li>
-    <button type="button">
-      <Fa icon={faMoon} />
+<MediaQuery query="(max-width: 800px)" let:matches>
+  {#if matches}
+    <button class="menu-toggle" type="button" on:click={() => {expandNavigation = !expandNavigation}}>
+      <Fa icon={expandNavigation ? faXmark : faBars} size="2x" />
     </button>
-  </li>
-</ul>
+    {#if expandNavigation}
+      <ul class="vertical-navigation">
+        <NavigationItem href="/" title="about" />
+        <NavigationItem href="/blog" title="blog" />
+        <NavigationItem href="/publications" title="publications" />
+        <NavigationItem href="/projects" title="projects" />
+        <NavigationItem href="/repositories" title="repositories" />
+        <NavigationItem href="/cv" title="cv" />
+        <NavigationItem href="/teaching" title="teaching" />
+        <NavigationItem href="/people" title="people" />
+        <NavigationItem href="/submenus" title="submenus" />
+        <li>
+          <button class="dark-mode-toggle" type="button">
+            <Fa icon={faMoon} />
+          </button>
+        </li>
+      </ul>
+    {/if}
+  {:else}
+    <ul>
+      <NavigationItem href="/" title="about" />
+      <NavigationItem href="/blog" title="blog" />
+      <NavigationItem href="/publications" title="publications" />
+      <NavigationItem href="/projects" title="projects" />
+      <NavigationItem href="/repositories" title="repositories" />
+      <NavigationItem href="/cv" title="cv" />
+      <NavigationItem href="/teaching" title="teaching" />
+      <NavigationItem href="/people" title="people" />
+      <NavigationItem href="/submenus" title="submenus" />
+      <li>
+        <button class="dark-mode-toggle" type="button">
+          <Fa icon={faMoon} />
+        </button>
+      </li>
+    </ul>
+  {/if}
+</MediaQuery>
 
 <style>
   button {
     background-color: inherit;
     border: 0;
+    cursor: pointer;
     padding: 0;
-    padding-left: 10px;
-    padding-top: 12px;
   }
 
   ul {
@@ -37,9 +65,24 @@
     margin-left: auto;
     margin-top: 0;
     padding-left: 0;
+    text-align: right;
   }
 
   li {
     list-style: none;
+  }
+
+  .dark-mode-toggle {
+    padding-left: 10px;
+    padding-top: 12px;
+  }
+
+  .menu-toggle {
+    margin-left: auto;
+    padding: .4rem;
+  }
+
+  .vertical-navigation {
+    flex-direction: column;
   }
 </style>
