cbd26c14642ea80ee833da8095ba23a96de13bdd
Marek Dědič <developer@dedic.eu>
2023-10-05 12:07:52 +0200

    Deduplicated navigation items

---
+++ src/lib/components/NavigationItems.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" strictEvents>
+  import { faMoon } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
+  import NavigationItem from "./NavigationItem.svelte";
+</script>
+
+<NavigationItem href="/" title="about" />
+<NavigationItem href="/blog" title="blog" />
+<NavigationItem href="/publications" title="publications" />
+<NavigationItem href="/projects" title="projects" />
+<NavigationItem href="/repositories" title="repositories" />
+<NavigationItem href="/cv" title="cv" />
+<NavigationItem href="/teaching" title="teaching" />
+<NavigationItem href="/people" title="people" />
+<NavigationItem href="/submenus" title="submenus" />
+<li>
+  <button type="button">
+    <Fa icon={faMoon} />
+  </button>
+</li>
+
+<style>
+  button {
+    background-color: inherit;
+    border: 0;
+    cursor: pointer;
+    padding: 0;
+    padding-left: 10px;
+    padding-top: 12px;
+  }
+
+  li {
+    list-style: none;
+  }
+</style>
