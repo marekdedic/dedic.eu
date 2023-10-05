36f92616f02e4db9016389de782d4677378dccbc
Marek Dědič <developer@dedic.eu>
2023-10-05 13:27:20 +0200

    Added dark mode toggling store and HTML element data

---
+++ src/lib/components/DarkModeToggle.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" strictEvents>
+  import { faMoon } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
+  import { theme } from "$lib/stores";
+</script>
+
+<li>
+  <button
+    type="button"
+    on:click={() => {
+      console.log("click");
+      theme.toggle();
+    }}
+  >
+    <Fa icon={faMoon} />
+  </button>
+</li>
+
+<style>
+  button {
+    background-color: inherit;
+    border: 0;
+    cursor: pointer;
+    padding: 0;
+    padding-left: 10px;
+    padding-top: 12px;
+  }
+
+  li {
+    list-style: none;
+  }
+</style>
--- src/lib/components/NavigationItems.svelte
+++ src/lib/components/NavigationItems.svelte
@@ -1,8 +1,7 @@
 <script lang="ts" strictEvents>
-  import { faMoon } from "@fortawesome/free-solid-svg-icons";
   import { createEventDispatcher } from "svelte";
-  import Fa from "svelte-fa";
 
+  import DarkModeToggle from "./DarkModeToggle.svelte";
   import NavigationItem from "./NavigationItem.svelte";
 
   const dispatch = createEventDispatcher<{ click: never }>();
@@ -71,23 +70,4 @@
     dispatch("click");
   }}
 />
-<li>
-  <button type="button">
-    <Fa icon={faMoon} />
-  </button>
-</li>
-
-<style>
-  button {
-    background-color: inherit;
-    border: 0;
-    cursor: pointer;
-    padding: 0;
-    padding-left: 10px;
-    padding-top: 12px;
-  }
-
-  li {
-    list-style: none;
-  }
-</style>
+<DarkModeToggle />
