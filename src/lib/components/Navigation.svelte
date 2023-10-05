c8271c97b150664ccff32442092edb51b78a3e78
Marek Dědič <developer@dedic.eu>
2023-10-05 12:07:52 +0200

    Deduplicated navigation items

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,9 +1,9 @@
 <script lang="ts" strictEvents>
-  import { faBars, faMoon, faXmark } from "@fortawesome/free-solid-svg-icons";
+  import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
   import Fa from "svelte-fa";
 
   import MediaQuery from "./MediaQuery.svelte";
-  import NavigationItem from "./NavigationItem.svelte";
+  import NavigationItems from "./NavigationItems.svelte";
 
   let expandNavigation = false;
 </script>
@@ -11,44 +11,23 @@
 <MediaQuery query="(max-width: 800px)" let:matches>
   {#if matches}
     <div>
-      <button class="menu-toggle" type="button" on:click={() => {expandNavigation = !expandNavigation}}>
+      <button
+        type="button"
+        on:click={() => {
+          expandNavigation = !expandNavigation;
+        }}
+      >
         <Fa icon={expandNavigation ? faXmark : faBars} size="2x" />
       </button>
       {#if expandNavigation}
         <ul class="vertical-navigation">
-          <NavigationItem href="/" title="about" />
-          <NavigationItem href="/blog" title="blog" />
-          <NavigationItem href="/publications" title="publications" />
-          <NavigationItem href="/projects" title="projects" />
-          <NavigationItem href="/repositories" title="repositories" />
-          <NavigationItem href="/cv" title="cv" />
-          <NavigationItem href="/teaching" title="teaching" />
-          <NavigationItem href="/people" title="people" />
-          <NavigationItem href="/submenus" title="submenus" />
-          <li>
-            <button class="dark-mode-toggle" type="button">
-              <Fa icon={faMoon} />
-            </button>
-          </li>
+          <NavigationItems />
         </ul>
       {/if}
     </div>
   {:else}
     <ul>
-      <NavigationItem href="/" title="about" />
-      <NavigationItem href="/blog" title="blog" />
-      <NavigationItem href="/publications" title="publications" />
-      <NavigationItem href="/projects" title="projects" />
-      <NavigationItem href="/repositories" title="repositories" />
-      <NavigationItem href="/cv" title="cv" />
-      <NavigationItem href="/teaching" title="teaching" />
-      <NavigationItem href="/people" title="people" />
-      <NavigationItem href="/submenus" title="submenus" />
-      <li>
-        <button class="dark-mode-toggle" type="button">
-          <Fa icon={faMoon} />
-        </button>
-      </li>
+      <NavigationItems />
     </ul>
   {/if}
 </MediaQuery>
@@ -58,10 +37,12 @@
     background-color: inherit;
     border: 0;
     cursor: pointer;
-    padding: 0;
+    margin-left: auto;
+    padding: 0.4rem;
   }
 
   div {
+    animation: max-height 5s ease;
     display: flex;
     flex-direction: column;
     margin-left: auto;
@@ -74,20 +55,6 @@
     margin-top: 0;
     padding-left: 0;
     text-align: right;
-  }
-
-  li {
-    list-style: none;
-  }
-
-  .dark-mode-toggle {
-    padding-left: 10px;
-    padding-top: 12px;
-  }
-
-  .menu-toggle {
-    margin-left: auto;
-    padding: .4rem;
   }
 
   .vertical-navigation {
