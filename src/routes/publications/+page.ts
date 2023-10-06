5b785e15b87b2a3b2ab412c88bf19e406e1e9e86
Marek Dědič <developer@dedic.eu>
2023-10-07 00:19:56 +0200

    Extracted publication loading into own function

--- src/routes/publications/+page.ts
+++ src/routes/publications/+page.ts
@@ -1,4 +1,4 @@
-import type { JsonPublication } from "$lib/types/JsonPublication";
+import { loadPublications } from "$lib/loadPublications";
 import type { PublicationParams } from "$lib/types/PublicationParams";
 
 import type { PageLoad } from "./$types";
@@ -6,12 +6,5 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad<{
   publications: Array<PublicationParams>;
 }> = async ({ fetch }) => {
-  const res = await fetch("/publications.json");
-  const publications = ((await res.json()) as Array<JsonPublication>)
-    .map((publication) => ({
-      ...publication,
-      date: new Date(publication.date),
-    }))
-    .sort((left, right) => right.date.getTime() - left.date.getTime());
-  return { publications };
+  return { publications: await loadPublications("/publications.json", fetch) };
 };
