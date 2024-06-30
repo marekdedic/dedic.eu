fdd817936a48b1d39aa8259c26648dd2ad6948d9
Marek Dědič <developer@dedic.eu>
2024-06-30 23:10:30 +0200

    Using rollup-plugin-htaccess for the Header directive

--- rollup-plugin-htaccess.config.ts
+++ rollup-plugin-htaccess.config.ts
@@ -16,6 +16,58 @@ const options: Partial<Options> = {
         ],
       },
     ],
+    Header: [
+      {
+        header: "Permissions-Policy",
+        always: true,
+        action: "set",
+        value: {
+          camera: {},
+          "display-capture": {},
+          fullscreen: { self: true },
+          geolocation: {},
+          microphone: {},
+          "web-share": {},
+        },
+      },
+      {
+        header: "Referrer-Policy",
+        always: true,
+        action: "set",
+        value: "same-origin",
+      },
+      {
+        header: "Strict-Transport-Security",
+        always: true,
+        action: "set",
+        value: {
+          maxAge: 31536000,
+          includeSubDomains: true,
+        },
+      },
+      {
+        header: "X-Content-Type-Options",
+        always: true,
+        action: "set",
+        value: {
+          nosniff: true,
+        },
+      },
+      {
+        header: "X-Frame-Options",
+        always: true,
+        action: "set",
+        value: "deny",
+      },
+      {
+        header: "X-Xss-Protection",
+        always: true,
+        action: "set",
+        value: {
+          mode: "block",
+        },
+      },
+    ],
     Options: {
       minus: ["Indexes"],
     },
