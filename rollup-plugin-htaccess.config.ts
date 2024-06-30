e1e734224489200f7e36ce5a4f1e9c1ce4aef547
Marek Dědič <developer@dedic.eu>
2024-06-30 22:51:27 +0200

    Using rollup-plugin-htaccess for the Options directive

--- rollup-plugin-htaccess.config.ts
+++ rollup-plugin-htaccess.config.ts
@@ -1,7 +1,14 @@
 import type { Options } from "rollup-plugin-htaccess";
 
+/* eslint-disable @typescript-eslint/naming-convention -- Most of these are things like header names */
 const options: Partial<Options> = {
   template: "htaccess-template.txt",
+  spec: {
+    Options: {
+      minus: ["Indexes"],
+    },
+  },
 };
+/* eslint-enable -- Re-enable naming-convention */
 
 export default options;
