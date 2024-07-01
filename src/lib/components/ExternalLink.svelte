f146bd91ad214c2afc931cdad24106d84ae1f808
Marek Dědič <developer@dedic.eu>
2024-07-01 19:00:58 +0200

    Made external links more visible with an icon

--- src/lib/components/ExternalLink.svelte
+++ src/lib/components/ExternalLink.svelte
@@ -1,4 +1,7 @@
 <script lang="ts" strictEvents>
+  import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
   export let href: string;
 
   interface $$Slots {
@@ -6,4 +9,7 @@
   }
 </script>
 
-<a {href} rel="noopener noreferrer" target="_blank"><slot /></a>
+<a {href} rel="noopener noreferrer" target="_blank">
+  <slot />
+  <Fa icon={faArrowUpRightFromSquare} size="0.6x" translateY={-0.1} />
+</a>
