4baa2977df4efbbe519147c4a4e7ef94731f2423
Marek Dědič <developer@dedic.eu>
2024-08-25 15:48:58 +0200

    Ordered objects

--- svelte.config.js
+++ svelte.config.js
@@ -2,27 +2,27 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      pages: "dist",
       assets: "dist",
       fallback: "index.html",
+      pages: "dist",
       precompress: false,
       strict: true,
     }),
     csp: {
-      mode: "hash",
       /* eslint-disable @typescript-eslint/naming-convention -- These are CSP directive names */
       directives: {
-        "upgrade-insecure-requests": true,
         "default-src": ["self"],
-        "object-src": ["none"],
-        "frame-src": ["https://docs.google.com/"],
         "font-src": ["self", "data:"],
+        "frame-src": ["https://docs.google.com/"],
+        "object-src": ["none"],
         "report-uri": ["https://marekdedic.report-uri.com/r/d/csp/enforce"],
+        "upgrade-insecure-requests": true,
       },
       /* eslint-enable @typescript-eslint/naming-convention */
+      mode: "hash",
     },
   },
+  preprocess: vitePreprocess(),
 };
