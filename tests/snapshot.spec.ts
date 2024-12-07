3e0e446b3fe80efed0c544b20a8d7c7c4acd45b1
Marek Dědič <developer@dedic.eu>
2024-12-07 19:23:08 +0100

    Masking publications where they are not needed

--- tests/snapshot.spec.ts
+++ tests/snapshot.spec.ts
@@ -7,35 +7,53 @@ test("main page", async ({ page }) => {
 
 test("dark mode toggling", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
   await page.getByLabel("Dark mode toggle").click();
   await expect(page.getByRole("document")).toHaveAttribute(
     "data-theme",
     "dark",
   );
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
   await page.reload();
   await expect(page.getByRole("document")).toHaveAttribute(
     "data-theme",
     "dark",
   );
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
   await page.getByLabel("Dark mode toggle").click();
   await expect(page.getByRole("document")).toHaveAttribute(
     "data-theme",
     "light",
   );
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
 });
 
 test("navigation", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
   await page
     .getByRole("list")
     .getByRole("link", { name: "publications" })
     .click();
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot({
+    // eslint-disable-next-line playwright/no-raw-locators -- No other way to locate publications
+    mask: [page.locator(".publication")],
+  });
 });
 
 test("publications page", async ({ page }) => {
