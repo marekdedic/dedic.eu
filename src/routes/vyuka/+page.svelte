7829444fc025a176996cd5d67283d153251ce1fe
Marek Dědič <developer@dedic.eu>
2025-09-25 14:37:01 +0200

    Added NI-NMS teaching

--- src/routes/vyuka/+page.svelte
+++ src/routes/vyuka/+page.svelte
@@ -24,11 +24,27 @@
 <ToC />
 
 <PageContent>
+  <Heading level={2}
+    >neuronové sítě, strojové učení a náhodnost – FIT, ZS 25/26</Heading
+  >
+
+  <p>
+    Cvičení předmětu neuronové sítě, strojové učení a náhodnost probíhají v sudé
+    týdny ve středy od 16:15 do 17:45 v místnosti T9:348 (viz <ExternalLink
+      href="https://timetable.fit.cvut.cz/new/courses/NI-NMS"
+      >rozvrh</ExternalLink
+    >), případně dle změn oznámených na přednáškách a cvičeních. Informace ke
+    zkoušce, podmínky splnění předmětu a klasifikace lze nalézt v <ExternalLink
+      href="https://courses.fit.cvut.cz/NI-NMS/index.html"
+      >FIT courses</ExternalLink
+    >.
+  </p>
+
   <Heading level={2}>teorie neuronových sítí – FIT, LS 24/25</Heading>
 
   <p>
     Cvičení předmětu teorie neuronových sítí probíhají v sudé týdny ve úterky od
-    14:30 do 16:0 v místnosti T9:351 (viz <ExternalLink
+    14:30 do 16:00 v místnosti T9:351 (viz <ExternalLink
       href="https://timetable.fit.cvut.cz/new/courses/NI-TNN"
       >rozvrh</ExternalLink
     >), případně dle změn oznámených na přednáškách a cvičeních. Informace ke
