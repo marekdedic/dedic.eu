0a5fd6c86af1a7d82ceeebb78d62e77a81199554
Marek Dědič <developer@dedic.eu>
2026-03-06 01:05:36 +0100

    Added support for math blocks with katex

---
+++ src/lib/components/InlineMath.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import katex from "katex";
+
+  interface Props {
+    displayMode?: boolean;
+    src: string;
+  }
+
+  let { displayMode = false, src }: Props = $props();
+
+  let outputElement: HTMLSpanElement | undefined = $state();
+
+  $effect(() => {
+    if (outputElement === undefined) {
+      return;
+    }
+    katex.render(src, outputElement, {
+      displayMode,
+      throwOnError: false,
+    });
+  });
+</script>
+
+<span bind:this={outputElement}></span>
