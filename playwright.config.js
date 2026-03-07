8825357d73f8f9045068c9a4f12db73d45d7593e
Marek Dědič <developer@dedic.eu>
2026-03-08 00:22:44 +0100

    Running tests with HTTPS

--- playwright.config.js
+++ playwright.config.js
@@ -26,12 +26,14 @@ export default defineConfig({
   retries: process.env.CI === undefined ? 0 : 2,
   testDir: "./tests",
   use: {
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "https://127.0.0.1:5173",
+    ignoreHTTPSErrors: true,
     trace: "on-first-retry",
   },
   webServer: {
     command: "npm run start -- --host",
+    ignoreHTTPSErrors: true,
     reuseExistingServer: false, // Breaks snapshots
-    url: "http://127.0.0.1:5173",
+    url: "https://127.0.0.1:5173",
   },
 });
