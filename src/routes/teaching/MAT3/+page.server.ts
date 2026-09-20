565e670420e06be5edf9991e701fb0b5a1c181a7
Marek Dědič <developer@dedic.eu>
2026-09-20 10:11:00 +0200

    Deduplicated course redirects

--- src/routes/teaching/MAT3/+page.server.ts
+++ src/routes/teaching/MAT3/+page.server.ts
@@ -1,12 +1,1 @@
-import { findCourseRoute } from "$lib/utils/findCourseRoute";
-import { redirect } from "@sveltejs/kit";
-
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ parent }) => {
-  const { teachingSpec } = await parent();
-  const course = teachingSpec.find((c) => c.slug === "MAT3");
-  if (course !== undefined) {
-    redirect(302, findCourseRoute(course));
-  }
-};
+export { load } from "$lib/utils/courseRedirect";
